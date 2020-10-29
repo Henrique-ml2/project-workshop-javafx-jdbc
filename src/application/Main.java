b120fc34813c58fcb4c391644b2ca22f84f7677d
Henrique Lira <henrique.melhado2@hotmail.com>
2020-10-29 16:29:49 -0200

    Main View

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,26 +1,29 @@
 package application;
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
+			// Instancia do objeto "new FXMLLoader"
+			// Agora é a INSTÂNCIA e não a chamada do MÉTODO
+			// Importante essa instanciação para manipulação da tela antes do carregamento dela
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+			Parent parent = loader.load(); // Carrega a View (tela)
+			Scene mainScene = new Scene(parent); // Instancia o objeto principal da View "AnchorPane"
+			primaryStage.setScene(mainScene); // Configura a Scene com o container "AnchorPane" e o que tiver nele
+			primaryStage.setTitle("Sample JavaFX application"); // Título da View
+			primaryStage.show(); // Mostra tudo (mesmo sem o mainScene mostrará uma tela branca - sem nada)
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+
 }
