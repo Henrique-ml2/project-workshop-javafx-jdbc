23921f39a36edbc2deeb240c9ef64b700a6f3728
Henrique Lira <henrique.melhado2@hotmail.com>
2020-10-29 21:58:52 -0200

    About View

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,23 +9,35 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
+	// Atributo criado para referenciar a Scene com método em outra classe, nesse caso a MainViewController
+	// Isso foi feito para poder manipular a Scene (o que aparece na View, não é o <ScrollPane>) 
+	private static Scene mainScene;
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
-			ScrollPane scrollPane = loader.load(); // Mudança para o "ScrollPane"
-			
-			// Macete para ficar ajustado com a View (tela)
-			scrollPane.setFitToHeight(true);
-			scrollPane.setFitToWidth(true);	
+			ScrollPane scrollPane = loader.load();
 
-			Scene mainScene = new Scene(scrollPane);
+			scrollPane.setFitToHeight(true);
+			scrollPane.setFitToWidth(true);
+
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
+	public static Scene getMainScene() {
+		return mainScene;	
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,11 +1,20 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable {
 
@@ -15,25 +24,71 @@ public class MainViewController implements Initializable {
 	private MenuItem menuItemDepartment;
 	@FXML
 	private MenuItem menuItemAbout;
-	
+
 	@FXML
 	public void onMenuItemSellerAction() {
 		System.out.println("onMenuItemSellerAction");
 	}
-	
+
 	@FXML
 	public void onMenuItemDepartmentAction() {
 		System.out.println("onMenuItemDepartmentAction");
 	}
-	
+
 	@FXML
 	public void onMenuItemAboutAction() {
-		System.out.println("onMenuItemAboutAction");
+		loadView("/gui/About.fxml");
 	}
 
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
 
+	}
+
+	// "absoluteName" pois é o caminho completo dos arquivos
+	// synchronized: para garantir que todo o processamento ocorra sem ser interrompido durante o multi-threading
+	private synchronized void loadView(String absoluteName) {
+		try {
+			
+			// Para carregar uma tela usa-se um objeto tipo FXMLLoader
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			
+			// Todas as outras Views serão apenas uma nova <VBox> para sobrepor a MainView
+			VBox newVBox = loader.load();
+			
+			// ---------- MUDAR PARA A VIEW DO ABOUT ---------- //
+			
+			// Aqui temos a Scene inteira
+			// - getMainScene(): método criado para referenciar a Scene criada na MainView
+			Scene mainScene = Main.getMainScene(); 
+
+			// 1) Referenciar a <VBox> principal
+			// - getRoot(): pegar o primeiro elemento da View	
+			// - (ScrollPane): para o compilador entender que estou pegando MESMO o <ScrollPane>
+			// - getContent(): pegar o <content> do <ScrollPane>
+			// - (VBox): para o compilador entender ao selecionar o <content>, selecionar o <VBox>
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent(); 
+			
+			// Guardar a barra de Menu <MenuBar> em um lugar para adicioná-lo de novo depois
+			// - getChildren(): para selecionar o <children>
+			// - get(0): para selecionar o <MenuBar> (primeira tag)
+			Node mainMenu = mainVBox.getChildren().get(0);
+			
+			// 2) Exclui todo(s) o(s) <children> da <VBox> principal
+			mainVBox.getChildren().clear();
+			
+			// 3) Adicionar a barra de Menu <MenuBar>
+			mainVBox.getChildren().add(mainMenu);
+			
+			// 4) Adicionar todo(s) o(s) <children> da <VBox> da View do About para o <VBox> principal
+			 
+			// Como só será trocado o(s) <children> a <VBox> principal, as outras telas começarão com <VBox> em seu código e não <ScrollPane>
+			// Por isso simplesmente "newBox.getChildren() diferente do passo 1), que percorreu tags até chegar na <VBox> principal "
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+
+		} catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 }
