94cc209668b53886aaa4b08a608e6a757ab8e726
Henrique Lira <henrique.melhado2@hotmail.com>
2020-10-29 17:33:03 -0200

    Main View design

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,23 +4,25 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			// Instancia do objeto "new FXMLLoader"
-			// Agora é a INSTÂNCIA e não a chamada do MÉTODO
-			// Importante essa instanciação para manipulação da tela antes do carregamento dela
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
-			Parent parent = loader.load(); // Carrega a View (tela)
-			Scene mainScene = new Scene(parent); // Instancia o objeto principal da View "AnchorPane"
-			primaryStage.setScene(mainScene); // Configura a Scene com o container "AnchorPane" e o que tiver nele
-			primaryStage.setTitle("Sample JavaFX application"); // Título da View
-			primaryStage.show(); // Mostra tudo (mesmo sem o mainScene mostrará uma tela branca - sem nada)
+			ScrollPane scrollPane = loader.load(); // Mudança para o "ScrollPane"
+			
+			// Macete para ficar ajustado com a View (tela)
+			scrollPane.setFitToHeight(true);
+			scrollPane.setFitToWidth(true);
+
+			Scene mainScene = new Scene(scrollPane);
+			primaryStage.setScene(mainScene);
+			primaryStage.setTitle("Sample JavaFX application");
+			primaryStage.show();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
