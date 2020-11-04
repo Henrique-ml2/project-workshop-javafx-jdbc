0d5a03dc47983ca0f1fa066eae80a66a71c7c9b3
Henrique Lira <henrique.melhado2@hotmail.com>
2020-11-04 19:17:05 -0200

    Adding database access

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -34,7 +34,6 @@ public class MainViewController implements Initializable {
 
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		// Agora a inicialização (ação de mostrar a lista na <TableView>) é feita no método
 		loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) -> {
 			controller.setDepartmentService(new DepartmentService());
 			controller.updateTableView();
@@ -43,9 +42,6 @@ public class MainViewController implements Initializable {
 
 	@FXML
 	public void onMenuItemAboutAction() {
-		// Como agora o método loadView() recebe função como parâmetro
-		// E como a View do About nõa tem nada para ser feito
-		// Faz-se uma função que não faz absolutamente nada
 		loadView("/gui/About.fxml", x -> {});
 	}
 
@@ -54,14 +50,6 @@ public class MainViewController implements Initializable {
 
 	}
 
-	// Agora o método é um tipo genérico, do tipo <T> (aceita função como parâmetro)
-	
-	// Isso foi feito para:
-	// 1) ter somente 1 versão do método, e não precisar criar outros loadView() diferentes, pois...
-	// 2) antes foi criado o loadView2() pois era com método que tinha uma AÇÃO (carregar a lista), e o loadView() era um método que NÃO tinha AÇÃO
-	// 3) antes caso fosse preciso carregar outra View que tivesse outra ação diferente, teria que criar por exemplo o loadView3() 
-	
-	// Então agora o método loadView() pode carregar as View's diferentes que tenham ou não uma ação - parametrizando com o Consumer<T>
 	private synchronized <T> void loadView(String absoluteName, Consumer<T> initializingAction) {
 		try {
 
@@ -76,8 +64,6 @@ public class MainViewController implements Initializable {
 			mainVBox.getChildren().add(mainMenu);
 			mainVBox.getChildren().addAll(newVBox.getChildren());
 			
-			// Executar a função que for passada como argumento 
-			// - getController(): agora retorna um Controller do tipo <T>, ou seja, retorna o tipo do Controller passado na expressão lambda
 			T controller = loader.getController();
 			initializingAction.accept(controller);
 			
@@ -85,26 +71,4 @@ public class MainViewController implements Initializable {
 			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
 		}
 	}
-	
-	/*
-	private synchronized void loadView2(String absoluteName) {
-		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-			VBox newVBox = loader.load();
-			Scene mainScene = Main.getMainScene();
-			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
-			Node mainMenu = mainVBox.getChildren().get(0);
-			mainVBox.getChildren().clear();
-			mainVBox.getChildren().add(mainMenu);
-			mainVBox.getChildren().addAll(newVBox.getChildren());
-			
-			// Passar essa inicialização como parâmetro do método loadView()
-			DepartmentListController controller = loader.getController();
-			controller.setDepartmentService(new DepartmentService());
-			controller.updateTableView();
-		} catch (IOException e) {
-			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
-		}
-	}
-	*/
 }
